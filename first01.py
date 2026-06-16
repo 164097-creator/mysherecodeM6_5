9714f50ff01b7e01949a0c873745864aa2e520bc
164097-creator <164097@wsk.ac.th>
2026-06-16 04:20:21 +0000

    first01.py

---
+++ first01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวนพื้นที่ของสามเหลี่ยม")
+h=float(input("Enter hight:"))
+b=float(input("Enter base:"))
+area=0.5hb
+print("พื้นที่ของสามเหลี่ยมเท่ากับ",area)
+print("จัดทำโดย ปาราเมศ สวัสดี เลขที่3")
